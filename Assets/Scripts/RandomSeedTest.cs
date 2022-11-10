cbbb072fecf7b216bfed6aa4391a36228c4cc75f
Beytullah KALAY <beytullah.kalay@outlook.com>
2022-11-10 14:27:30 +0300

    add: random seed test

---
+++ Assets/Scripts/RandomSeedTest.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomSeedTest : MonoBehaviour
+{
+    public string texts = "deneme metini";
+
+    private void Awake()
+    {
+        GenerateRandomSeed();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetSeed(texts.GetHashCode());
+
+            Debug.Log(Random.Range(-100, 100));
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            GenerateRandomSeed();
+        }
+    }
+
+    private void SetSeed(int state = 0)
+    {
+        Random.InitState(state);
+    }
+
+    private void GenerateRandomSeed()
+    {
+        int tempSeed = (int)System.DateTime.Now.Ticks;
+        Random.InitState(tempSeed);
+
+        Debug.Log(Random.Range(-100, 100));
+    }
+}
