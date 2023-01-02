ef9de51a680f6e9eb7ec803340a832d9ee08ed58
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-01-02 18:56:02 +0300

    add: recorder

---
+++ Assets/Scripts/HandAnimations/HandAnimation.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class HandAnimation : MonoBehaviour
+{
+    [SerializeField] private float moveY = 1;
+    [SerializeField] private float moveDuration = 1;
+    [SerializeField] private Ease moveEase;
+
+    private Tween _tween;
+
+    private void OnEnable()
+    {
+        _tween?.Kill();
+
+        _tween = transform.DOMoveY(moveY, moveDuration).SetEase(moveEase).SetLoops(-1, LoopType.Yoyo);
+    }
+}
